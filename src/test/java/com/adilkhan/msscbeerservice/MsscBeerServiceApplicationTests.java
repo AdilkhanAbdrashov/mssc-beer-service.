ab10b38b9bc690042a21d162e7609fd0bb0bc5ac
Adilkhan Abdrashov <67748974+AdilkhanAbdrashov@users.noreply.github.com>
2022-08-02 16:53:20 +0600

    initial project add. Closes#1

---
+++ src/test/java/com/adilkhan/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adilkhan.msscbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
