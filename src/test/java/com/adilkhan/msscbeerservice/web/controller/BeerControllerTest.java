337d91b2944d47e961c91f6305cb36c0dee48b0e
Adilkhan Abdrashov <67748974+AdilkhanAbdrashov@users.noreply.github.com>
2022-08-11 01:56:26 +0600

    adding beer repository closes#6

--- src/test/java/com/adilkhan/msscbeerservice/web/controller/BeerControllerTest.java
+++ src/test/java/com/adilkhan/msscbeerservice/web/controller/BeerControllerTest.java
@@ -5,7 +5,11 @@ import com.adilkhan.msscbeerservice.web.model.BeerDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.TestComponent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.UUID;
